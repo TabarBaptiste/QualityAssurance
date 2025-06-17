a59dca18c13925a6e1fd4a15b9af9a4a376f338c
Anilzer <abayomiandil62@gmail.com>
2025-06-17 16:54:14 +0200

    tests approfondis

--- test_radars.py
+++ test_radars.py
@@ -1,7 +1,7 @@
 import unittest
 import pandas as pd
 
-# Chemin vers le fichier
+# Chemin vers le fichier csv
 PATH = "data/jeu-de-donnees-liste-des-radars-fixes-en-france-2024-.csv"
 
 class TestChargementFichier(unittest.TestCase):
@@ -13,6 +13,19 @@ class TestChargementFichier(unittest.TestCase):
         except Exception as e:
             self.fail(f"Erreur lors du chargement du fichier CSV : {e}")
 
+class TestValeursFichier(unittest.TestCase):
+    def setUp(self):
+        self.df = pd.read_csv(PATH, sep=";", encoding="ISO-8859-1")
+
+    def test_latitude_valide(self):
+        """Test si toutes les latitudes sont comprises entre -90 et 90 degrés."""
+        self.assertTrue(self.df["Latitude"].between(-90, 90).all(), "Certaines latitudes sont hors limites.")
+
+    def test_longitude_valide(self):
+        """Test si toutes les longitudes sont comprises entre -180 et 180 degrés."""
+        self.assertTrue(self.df["Longitude"].between(-180, 180).all(), "Certaines longitudes sont hors limites.")
+
+
 class TestStructureFichier(unittest.TestCase):
     def setUp(self):
         self.df = pd.read_csv(PATH, sep=";", encoding="ISO-8859-1")
@@ -20,7 +33,7 @@ class TestStructureFichier(unittest.TestCase):
     def test_colonnes_existantes(self):
         """Vérifie que les colonnes attendues sont présentes dans le fichier CSV."""
         colonnes_attendues = [
-            "Numéro de radar ",
+            "Numéro de radar",
             "Latitude",
             "Longitude",
             "VMA"
